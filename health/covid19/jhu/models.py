cb81d486347d904e29cc96d0f5568793e6e3565a
Makhosandile <mpnyamela@gmail.com>
2021-04-24 07:27:58 +0200

    Added JHB Models

---
+++ health/covid19/jhu/models.py
@@ -0,0 +1,24 @@
+from pydantic import BaseModel, Field
+
+
+class CovidModel(BaseModel):
+    """Dataclass acts as a Model for Covid data
+    """
+
+    id: str = Field(..., alias="OBJECTID")
+    country: str = Field(..., alias="Country_Region")
+    confirmed: int = Field(0, alias="Confirmed")
+    active: int = Field(0, alias="Active")
+    deaths: int = Field(0, alias="Deaths")
+    recovered: int = Field(None, alias="Recovered")
+    latitude: float = Field(None, alias="Lat")
+    longitude: float = Field(None, alias="Long_")
+    last_update: int = Field(0, alias="Last_Update")
+
+
+class CountryModel(BaseModel):
+    """Dataclass acts as a Model for Countries data
+    """
+
+    id: str = Field(..., alias="OBJECTID")
+    name: str = Field(..., alias="Country_Region")
